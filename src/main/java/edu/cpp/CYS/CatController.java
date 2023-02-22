ba37e980fa0561c5676219996766957fd0e8643f
semava <semavargas09@yahoo.com>
2023-02-22 11:30:57 -0800

    Added cat page

---
+++ src/main/java/edu/cpp/CYS/CatController.java
@@ -0,0 +1,49 @@
+package edu.cpp.CYS;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.google.gson.Gson;
+
+
+
+@Controller
+public class CatController
+{
+	private final static String API_ROOT_CAT = "http://aws.random.cat/meow";
+	
+	@GetMapping("/cat")
+	public String cat(Model model)
+	{
+		String link = "http://lightwidget.com/wp-content/uploads/local-file-not-found.png";
+		Gson g = new Gson();
+		URL u;
+		try
+		{
+			u = new URL(API_ROOT_CAT);
+			HttpURLConnection c = (HttpURLConnection) u.openConnection();
+			InputStreamReader r = new InputStreamReader(c.getInputStream());
+			catData cat = g.fromJson(r,  catData.class);
+			r.close();
+			link = cat.file;
+		}
+		catch(IOException | NullPointerException e)
+		{
+			e.printStackTrace();
+			
+		}
+		model.addAttribute("cat", link);
+		return "cat";
+	}
+	
+	private static class catData
+	{
+		String file = "http://lightwidget.com/wp-content/uploads/local-file-not-found.png";
+	}
+}
--- src/main/java/edu/cpp/CYS/CysArtAppApplication.java
+++ src/main/java/edu/cpp/CYS/CysArtAppApplication.java
@@ -1,15 +1,24 @@
 package edu.cpp.CYS;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 @SpringBootApplication
 @RestController
+
 public class CysArtAppApplication {
 
 	public static void main(String[] args) 
@@ -57,5 +66,10 @@ public class CysArtAppApplication {
 	public String sayHello(@RequestParam(value = "email", defaultValue = "n/a") String email) {
 		return String.format("Thank you for joining, %s.", email);
 	}
+	
+	
+	
+	
+	
 
 }
