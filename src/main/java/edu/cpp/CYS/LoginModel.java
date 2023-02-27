6491ff2b6e24c58a0b9eb455f4ddd685da82cd00
Yuliana Joya <yulianajoya@gmail.com>
2023-02-27 01:58:09 -0800

    Added Login Class

---
+++ src/main/java/edu/cpp/CYS/LoginModel.java
@@ -0,0 +1,36 @@
+package edu.cpp.CYS;
+
+public class LoginModel{
+   private String username;
+   private String password;
+
+   public LoginModel() {
+      super();
+   }
+
+   public LoginModel(String username, String password) {
+      this.username = username;
+      this.password = password;
+   }
+
+   @Override
+   public String toString() {
+      return "LoginModel [username=" + username + ", password=" + password + "]";
+   }
+
+   public void setUsername(String username) {
+      this.username = username;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   public String getUsername() {
+      return username;
+   }
+   
+   public String getPassword() {
+      return password;
+   }
+}
