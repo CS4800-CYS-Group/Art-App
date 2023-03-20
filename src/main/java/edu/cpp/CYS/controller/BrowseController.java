b3a59eb05bfbd0142941cbc4bc35ca2a5d92b38f
cduong2020 <67450183+cduong2020@users.noreply.github.com>
2023-03-19 18:20:41 -0700

    Update BrowseController.java
    forgot to commit, add linking to browse controller

--- src/main/java/edu/cpp/CYS/controller/BrowseController.java
+++ src/main/java/edu/cpp/CYS/controller/BrowseController.java
@@ -1,2 +1,16 @@
-package edu.cpp.CYS.controller;public class BrowseController {
+package edu.cpp.CYS.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import edu.cpp.CYS.model.User;
+
+
+@Controller
+public class BrowseController {
+    @GetMapping("/browse")
+    public String getLogin(){
+        return "socialMediaView.html";
+
+    }
 }
