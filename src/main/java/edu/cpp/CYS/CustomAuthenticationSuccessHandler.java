86e68404397e64dc123aedd1b0cd95b76c55ea5b
Yuliana Joya <yulianajoya@gmail.com>
2023-04-07 16:21:39 -0700

    Makes it so access isn't denied when a user logs in

---
+++ src/main/java/edu/cpp/CYS/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,19 @@
+package edu.cpp.CYS;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//Makes it so access isn't denied when a user logs in
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+   @Override
+   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+       response.sendRedirect("/us/" + authentication.getName());
+   }
+}
