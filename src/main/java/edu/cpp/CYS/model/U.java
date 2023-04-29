7be14232e84c9bd2fb2c8d94a15ec60ce1630796
Yuliana Joya <yulianajoya@gmail.com>
2023-04-29 13:27:26 -0700

    Added a list for following and followers

--- src/main/java/edu/cpp/CYS/model/U.java
+++ src/main/java/edu/cpp/CYS/model/U.java
@@ -1,5 +1,6 @@
 package edu.cpp.CYS.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.*;
@@ -32,10 +33,20 @@ public class U
     @OneToMany(mappedBy="user", cascade=CascadeType.ALL)
     private List<Photo> photos;
 
+    private byte[] profilePicture;
+    private String bio;
+    @ManyToMany
+    @JoinTable(name = "follow",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "follower_id"))
+    private List<U> followers = new ArrayList<>();
+    @ManyToMany(mappedBy = "followers")
+    private List<U> following = new ArrayList<>();
+
 	public U() {
     }
 
-	public U(Long id, String email, String username, String password, String firstName, String lastName, List<Photo> photos) {
+	public U(Long id, String email, String username, String password, String firstName, String lastName, List<Photo> photos, byte[] profilePicture, String bio, List<U> followers, List<U> following) {
         this.id = id;
 		this.email = email;
         this.username = username;
@@ -43,7 +54,43 @@ public class U
 		this.firstName = firstName;
 		this.lastName = lastName;
         this.photos = photos;
+        this.profilePicture = profilePicture;
+        this.bio = bio;
+        this.followers = followers;
+        this.following = following;
 	}
+
+    public byte[] getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(byte[] profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public List<U> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(List<U> followers) {
+        this.followers = followers;
+    }
+
+    public List<U> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(List<U> following) {
+        this.following = following;
+    }
 
     public String getUsername() {
         return username;
